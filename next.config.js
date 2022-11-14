d7b6510fce14e0f346fc95477d81fb1e0a5408d8
radnive <radnive.dev@gmail.com>
2022-11-13 23:56:02 -0800

    Add localization to app (#2)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["en", "fa"],
+    defaultLocale: "en"
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
+import '../styles/fonts.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { IntlProvider } from 'react-intl';
+
+import en from '../lang/en.json';
+import fa from '../lang/fa.json';
+const messages = { en, fa };
+
+const getDirection = (locale: string) => (locale === 'en')? 'ltr' : 'rtl';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { locale = 'en' } = useRouter();
+
+  return (
+    <IntlProvider locale={locale} messages={messages[locale as keyof typeof messages]}>
+      <Component {...pageProps} dir={getDirection(locale)} />
+    </IntlProvider>
+  );
 }
