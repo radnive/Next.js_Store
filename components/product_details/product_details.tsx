dbb7a78a640f85a1662f6464e9d1b46318be04b6
radnive <radnive.dev@gmail.com>
2022-11-22 03:06:10 -0800

    Fix Product details section issue: Add buttons layout to outside of main section (#23)

--- components/product_details/product_details.tsx
+++ components/product_details/product_details.tsx
@@ -9,6 +9,7 @@ import LikeButton from '../like_button/like_button';
 import ShareButton from '../share_button/share_button';
 import FullScreenButton from '../full_screen_button/full_screen_button';
 import { TLink } from '../transition/transition';
+
 interface ProductDetailsProps {
   product: {
     id: string,
@@ -46,6 +47,17 @@ const ProductDetails: FC<ProductDetailsProps> = ({product}) => {
       }
     );
 
+    tl?.fromTo('#product_details__outside_buttons',
+      {
+        y: '100%'
+      },
+      {
+        y: 0,
+        duration: .5
+      },
+      '<0%'
+    );
+
     tl?.fromTo('#product_details__image',
       {
         scale: 1.12
@@ -79,64 +91,75 @@ const ProductDetails: FC<ProductDetailsProps> = ({product}) => {
   }, [mainRef]);
 
   return (
-    <section ref={mainRef} className={styles.main}>
-      <div className={styles.image_container}>
-        <img
-          id='product_details__image'
-          className={styles.image}
-          src={product.image}
-          alt={product.name}
-          lang={intl.locale} />
+    <>
+      <section ref={mainRef} className={styles.main}>
+        <div className={styles.image_container}>
+          <img
+            id='product_details__image'
+            className={styles.image}
+            src={product.image}
+            alt={product.name}
+            lang={intl.locale} />
 
-          <ShareButton className={styles.share_button} lang={intl.locale} />
-          <FullScreenButton src={product.image} className={styles.full_screen_button} lang={intl.locale} />
-      </div>
-
-      <div className={styles.details_container}>
-        <div className={styles.details}>
-          <header ref={headerRef}>
-            <ul className={styles.details__direction}>
-              <li className={styles.details__direction__link}>
-                <TLink href='/'>{ intl.formatMessage({ id: 'product.details.home' }) }</TLink>
-              </li>
-
-              <li>{'>'}</li>
-
-              <li className={styles.details__direction__link}>{ product.category }</li>
-
-              <li>{'>'}</li>
-
-              <li className={styles.details__direction__link}>{ product.brand }</li>
-            </ul>
-
-            <h1 className={styles.details__name}>{product.name}</h1>
-          </header>
-
-          <footer ref={footerRef}>
-            <ColorSelector colors={product.colors} />
-
-            {
-              (product.sizes.length > 0)?
-              (
-                <>
-                  <Spacer height='2rem' />
-                  <SizeSelector sizes={product.sizes} />
-                </>
-              ) : ''
-            }
-
-            <div className={styles.buttons}>
-              <button className={styles.buttons__purchase}>
-                { intl.formatMessage({ id: 'button.purchase.text' }) }
-                <b>${ product.price }</b>
-              </button>
-
-              <LikeButton className={styles.buttons__like_button} liked={product.isFavorite} />
-            </div>
-          </footer>
+            <ShareButton className={styles.share_button} lang={intl.locale} />
+            <FullScreenButton src={product.image} className={styles.full_screen_button} lang={intl.locale} />
         </div>
+
+        <div className={styles.details_container}>
+          <div className={styles.details}>
+            <header ref={headerRef}>
+              <ul className={styles.details__direction}>
+                <li className={styles.details__direction__link}>
+                  <TLink href='/'>{ intl.formatMessage({ id: 'product.details.home' }) }</TLink>
+                </li>
+
+                <li>{'>'}</li>
+
+                <li className={styles.details__direction__link}>{ product.category }</li>
+
+                <li>{'>'}</li>
+
+                <li className={styles.details__direction__link}>{ product.brand }</li>
+              </ul>
+
+              <h1 className={styles.details__name}>{product.name}</h1>
+            </header>
+
+            <footer ref={footerRef}>
+              <ColorSelector colors={product.colors} />
+
+              {
+                (product.sizes.length > 0)?
+                (
+                  <>
+                    <Spacer height='2rem' />
+                    <SizeSelector sizes={product.sizes} />
+                  </>
+                ) : ''
+              }
+
+              <div className={styles.buttons} data-button-type='inside'>
+                <button className={styles.buttons__purchase}>
+                  { intl.formatMessage({ id: 'button.purchase.text' }) }
+                  <b>${ product.price }</b>
+                </button>
+
+                <LikeButton className={styles.buttons__like_button} liked={product.isFavorite} />
+              </div>
+            </footer>
+          </div>
+        </div>
+      </section>
+
+      <div id='product_details__outside_buttons' className={styles.buttons} data-button-type='outside'>
+        <button className={styles.buttons__purchase}>
+          { intl.formatMessage({ id: 'button.purchase.text' }) }
+          <b>${ product.price }</b>
+        </button>
+
+        <LikeButton className={styles.buttons__like_button} liked={product.isFavorite} />
       </div>
-    </section>
+    </>
   );
 }
 
